04c099f2f4a4341038ba1ddbc5c8837e715e673b
fcd <1352288+femnad@users.noreply.github.com>
2026-05-17 23:40:58 +0100

    firmware25 rgb lighting fixes

--- keyboards/zsa/moonlander/keymaps/foolmap/fn.c
+++ keyboards/zsa/moonlander/keymaps/foolmap/fn.c
@@ -23,18 +23,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define LEFT_GUI_INDEX 4
 #define RIGHT_GUI_INDEX 40
 
-void clear_colors(void) {
-  rgb_matrix_set_color(LEFT_SHIFT_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(LEFT_CTRL_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(RIGHT_CTRL_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(LEFT_ALT_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(RIGHT_ALT_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(LEFT_GUI_INDEX, 0, 0, 0);
-  rgb_matrix_set_color(RIGHT_GUI_INDEX, 0, 0, 0);
-}
+bool rgb_matrix_indicators_user(void) {
+  uint8_t mods = get_oneshot_mods();
+  uint8_t locked_mods = get_oneshot_locked_mods();
 
-void oneshot_mods_changed_user(uint8_t mods) {
   if (mods & MOD_MASK_SHIFT) {
     rgb_matrix_set_color(LEFT_SHIFT_INDEX, 128, 0, 0);
     rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 128, 0, 0);
@@ -51,31 +43,25 @@ void oneshot_mods_changed_user(uint8_t mods) {
     rgb_matrix_set_color(LEFT_GUI_INDEX, 0, 100, 200);
     rgb_matrix_set_color(RIGHT_GUI_INDEX, 0, 100, 200);
   }
-  if (!mods) {
-    clear_colors();
-  }
-}
 
-void oneshot_locked_mods_changed_user(uint8_t mods) {
-  if (mods & MOD_MASK_SHIFT) {
+  if (locked_mods & MOD_MASK_SHIFT) {
     rgb_matrix_set_color(LEFT_SHIFT_INDEX, 255, 0, 0);
     rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 255, 0, 0);
   }
-  if (mods & MOD_MASK_CTRL) {
+  if (locked_mods & MOD_MASK_CTRL) {
     rgb_matrix_set_color(LEFT_CTRL_INDEX, 0, 255, 0);
     rgb_matrix_set_color(RIGHT_CTRL_INDEX, 0, 255, 0);
   }
-  if (mods & MOD_MASK_ALT) {
+  if (locked_mods & MOD_MASK_ALT) {
     rgb_matrix_set_color(LEFT_ALT_INDEX, 0, 0, 255);
     rgb_matrix_set_color(RIGHT_ALT_INDEX, 0, 0, 255);
   }
-  if (mods & MOD_MASK_GUI) {
+  if (locked_mods & MOD_MASK_GUI) {
     rgb_matrix_set_color(LEFT_GUI_INDEX, 100, 200, 255);
     rgb_matrix_set_color(RIGHT_GUI_INDEX, 100, 200, 255);
   }
-  if (!mods) {
-    clear_colors();
-  }
+
+  return true;
 }
 
 void keyboard_post_init_user(void) {
