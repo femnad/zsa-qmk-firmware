4be4ae60980c1b1b1a81b6706d8ccf2202b6e0fa
fcd <1352288+femnad@users.noreply.github.com>
2026-05-18 00:04:10 +0100

    add missing rgb_matrix_indicators_user

--- keyboards/zsa/moonlander/keymaps/foolmap-tr/keymap.c
+++ keyboards/zsa/moonlander/keymaps/foolmap-tr/keymap.c
@@ -178,3 +178,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define RIGHT_ALT_INDEX 70
 #define LEFT_GUI_INDEX 4
 #define RIGHT_GUI_INDEX 40
+
+bool rgb_matrix_indicators_user(void) {
+  uint8_t mods = get_oneshot_mods();
+  uint8_t locked_mods = get_oneshot_locked_mods();
+
+  if (mods & MOD_MASK_SHIFT) {
+    rgb_matrix_set_color(LEFT_SHIFT_INDEX, 128, 0, 0);
+    rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 128, 0, 0);
+  }
+  if (mods & MOD_MASK_CTRL) {
+    rgb_matrix_set_color(LEFT_CTRL_INDEX, 0, 128, 0);
+    rgb_matrix_set_color(RIGHT_CTRL_INDEX, 0, 128, 0);
+  }
+  if (mods & MOD_MASK_ALT) {
+    rgb_matrix_set_color(LEFT_ALT_INDEX, 0, 0, 128);
+    rgb_matrix_set_color(RIGHT_ALT_INDEX, 0, 0, 128);
+  }
+  if (mods & MOD_MASK_GUI) {
+    rgb_matrix_set_color(LEFT_GUI_INDEX, 0, 100, 200);
+    rgb_matrix_set_color(RIGHT_GUI_INDEX, 0, 100, 200);
+  }
+
+  if (locked_mods & MOD_MASK_SHIFT) {
+    rgb_matrix_set_color(LEFT_SHIFT_INDEX, 255, 0, 0);
+    rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 255, 0, 0);
+  }
+  if (locked_mods & MOD_MASK_CTRL) {
+    rgb_matrix_set_color(LEFT_CTRL_INDEX, 0, 255, 0);
+    rgb_matrix_set_color(RIGHT_CTRL_INDEX, 0, 255, 0);
+  }
+  if (locked_mods & MOD_MASK_ALT) {
+    rgb_matrix_set_color(LEFT_ALT_INDEX, 0, 0, 255);
+    rgb_matrix_set_color(RIGHT_ALT_INDEX, 0, 0, 255);
+  }
+  if (locked_mods & MOD_MASK_GUI) {
+    rgb_matrix_set_color(LEFT_GUI_INDEX, 100, 200, 255);
+    rgb_matrix_set_color(RIGHT_GUI_INDEX, 100, 200, 255);
+  }
+
+  return true;
+}
