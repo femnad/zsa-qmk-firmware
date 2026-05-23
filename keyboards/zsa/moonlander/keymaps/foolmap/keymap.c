ba78ba6ca9ecb614b01cc6564efb8b9ae418d085
fcd <1352288+femnad@users.noreply.github.com>
2026-05-23 23:17:12 +0100

    fix swapped keys

--- keyboards/zsa/moonlander/keymaps/foolmap/keymap.c
+++ keyboards/zsa/moonlander/keymaps/foolmap/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  * | MPLY  |   '   |   ,   |   .   |   P   |   Y   |  TAB  |  ENT  |   F   |   G   |   C   |   R   |   L   | VOLD  |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * | @MOVE |   A   | HOMEO | HOMEE | HOMEH | INDXI | ^LSFT | ^RSFT | INDXD | HOMEH | HOMET | HOMEN |   S   | @MOVE |
+ * | @MOVE |   A   | HOMEO | HOMEE | HOMEU | INDXI | ^LSFT | ^RSFT | INDXD | HOMEH | HOMET | HOMEN |   S   | @MOVE |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * | CLEAR |   ;   |   Q   |   J   |   K   | DIAGB |               | DIAGX |   M   |   W   |   V   |   Z   | CLEAR |
+ * | CLEAR |   ;   |   Q   |   J   |   K   | DIAGX |               | DIAGB |   M   |   W   |   V   |   Z   | CLEAR |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  * | ^LGUI | HOME  | PGUP  | PGDN  |  END  | @SYMB |               | @SYMB | LEFT  | DOWN  |  UP   | RIGHT | ^RGUI |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_moonlander(
         KC_MUTE,       KC_1,          KC_2,          KC_3,          KC_4,          KC_5,      KC_ESC,        KC_BSPC,       KC_6,    KC_7,   KC_8,     KC_9,          KC_0, KC_VOLU,
         KC_MPLY,       KC_QUOT,       KC_COMM,       KC_DOT,        KC_P,          KC_Y,      KC_TAB,        KC_ENT,        KC_F,    KC_G,   KC_C,     KC_R,          KC_L, KC_VOLD,
-        OSL(MOVE),     KC_A,          HOME_O,        HOME_E,        HOME_H,        INDX_I,    OSM(MOD_LSFT), OSM(MOD_RSFT), INDX_D,  HOME_H, HOME_T,   HOME_N,        KC_S, OSL(MOVE),
-        CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          DIAG_B,    DIAG_X,        KC_M,          KC_W,    KC_V,   KC_Z,     CLEAR,
+        OSL(MOVE),     KC_A,          HOME_O,        HOME_E,        HOME_U,        INDX_I,    OSM(MOD_LSFT), OSM(MOD_RSFT), INDX_D,  HOME_H, HOME_T,   HOME_N,        KC_S, OSL(MOVE),
+        CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          DIAG_X,    DIAG_B,        KC_M,          KC_W,    KC_V,   KC_Z,     CLEAR,
         OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        OSL(SYMB), OSL(SYMB),     KC_LEFT,       KC_DOWN, KC_UP,  KC_RIGHT, OSM(MOD_RGUI),
         KC_SPC,        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
 ),
