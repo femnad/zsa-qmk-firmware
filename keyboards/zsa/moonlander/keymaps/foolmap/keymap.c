210c9b87393cafddc8854eae2e609db8149c95da
fcd <1352288+femnad@users.noreply.github.com>
2026-05-23 22:43:06 +0100

    use config to add home row mods

--- keyboards/zsa/moonlander/keymaps/foolmap/keymap.c
+++ keyboards/zsa/moonlander/keymaps/foolmap/keymap.c
@@ -8,10 +8,22 @@
 
 #define DIAG_B MT(MOD_RSFT, KC_B)
 #define DIAG_X MT(MOD_LSFT, KC_X)
+#define HOME_E MT(MOD_LALT, KC_E)
 #define HOME_H MT(MOD_RCTL, KC_H)
+#define HOME_N MT(MOD_RGUI, KC_N)
+#define HOME_O MT(MOD_LGUI, KC_O)
+#define HOME_T MT(MOD_RALT, KC_T)
 #define HOME_U MT(MOD_LCTL, KC_U)
 #define INDX_I LT(INDX, KC_I)
 #define INDX_D LT(INDX, KC_D)
+#define LEFT_SHIFT_INDEX 31
+#define RIGHT_SHIFT_INDEX 67
+#define LEFT_CTRL_INDEX 33
+#define RIGHT_CTRL_INDEX 69
+#define LEFT_ALT_INDEX 34
+#define RIGHT_ALT_INDEX 70
+#define LEFT_GUI_INDEX 4
+#define RIGHT_GUI_INDEX 40
 
 enum custom_keycodes {
   CLEAR = SAFE_RANGE
@@ -25,23 +37,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  * | MPLY  |   '   |   ,   |   .   |   P   |   Y   |  TAB  |  ENT  |   F   |   G   |   C   |   R   |   L   | VOLD  |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * | @MOVE |   A   |   O   |   E   |   U   |   I   | ^LSFT | ^RSFT |   D   |   H   |   T   |   N   |   S   | @MOVE |
+ * | @MOVE |   A   | HOMEO | HOMEE | HOMEH | INDXI | ^LSFT | ^RSFT | INDXD | HOMEH | HOMET | HOMEN |   S   | @MOVE |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * | CLEAR |   ;   |   Q   |   J   |   K   |   X   |               |   B   |   M   |   W   |   V   |   Z   | CLEAR |
+ * | CLEAR |   ;   |   Q   |   J   |   K   | DIAGB |               | DIAGX |   M   |   W   |   V   |   Z   | CLEAR |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  * | ^LGUI | HOME  | PGUP  | PGDN  |  END  | @SYMB |               | @SYMB | LEFT  | DOWN  |  UP   | RIGHT | ^RGUI |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  *                         |  SPC  | ^LCTL | ^LALT |               | ^RALT | ^RCTL |  SPC  |
  *                         +-------+-------+-------+               +-------+-------+-------+
- *
  */
 
 [BASE] = LAYOUT_moonlander(
-        KC_MUTE,       KC_1,          KC_2,          KC_3,          KC_4,          KC_5,      KC_ESC,        KC_BSPC,       KC_6,    KC_7,  KC_8,     KC_9,          KC_0, KC_VOLU,
-        KC_MPLY,       KC_QUOT,       KC_COMM,       KC_DOT,        KC_P,          KC_Y,      KC_TAB,        KC_ENT,        KC_F,    KC_G,  KC_C,     KC_R,          KC_L, KC_VOLD,
-        OSL(MOVE),     KC_A,          KC_O,          KC_E,          HOME_U,        INDX_I,    OSM(MOD_LSFT), OSM(MOD_RSFT), INDX_D,  HOME_H, KC_T,    KC_N,          KC_S, OSL(MOVE),
-        CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          DIAG_X,    DIAG_B,        KC_M,          KC_W,    KC_V,  KC_Z,     CLEAR,
-        OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        OSL(SYMB), OSL(SYMB),     KC_LEFT,       KC_DOWN, KC_UP, KC_RIGHT, OSM(MOD_RGUI),
+        KC_MUTE,       KC_1,          KC_2,          KC_3,          KC_4,          KC_5,      KC_ESC,        KC_BSPC,       KC_6,    KC_7,   KC_8,     KC_9,          KC_0, KC_VOLU,
+        KC_MPLY,       KC_QUOT,       KC_COMM,       KC_DOT,        KC_P,          KC_Y,      KC_TAB,        KC_ENT,        KC_F,    KC_G,   KC_C,     KC_R,          KC_L, KC_VOLD,
+        OSL(MOVE),     KC_A,          HOME_O,        HOME_E,        HOME_H,        INDX_I,    OSM(MOD_LSFT), OSM(MOD_RSFT), INDX_D,  HOME_H, HOME_T,   HOME_N,        KC_S, OSL(MOVE),
+        CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          DIAG_B,    DIAG_X,        KC_M,          KC_W,    KC_V,   KC_Z,     CLEAR,
+        OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        OSL(SYMB), OSL(SYMB),     KC_LEFT,       KC_DOWN, KC_UP,  KC_RIGHT, OSM(MOD_RGUI),
         KC_SPC,        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
 ),
 
@@ -59,7 +70,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  *                         |       |       |       |               |       |       |       |
  *                         +-------+-------+-------+               +-------+-------+-------+
- *
  */
 
 [SYMB] = LAYOUT_moonlander(
@@ -96,25 +106,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,  _______, _______, _______
 ),
 
+/* Layer: INDX
+ * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+ * |       |       |       |       |       |       |       |       |       |       |       |       |       |       |
+ * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+ * |       |       |       |  MSU  |       |  ESC  |       |       | BSPC  |       |  UP   |       |       |       |
+ * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+ * |       |       |  MSL  |  MSD  |  MSR  |  TAB  |       |       |  ENT  | LEFT  | DOWN  | RIGHT |       |       |
+ * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+ * |       |       |       |       |       |       |               |       |  WHL  |  WHR  |       |       |       |
+ * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
+ * |       |       |       |       |       |       |               |       |       |       |       |       |       |
+ * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
+ *                         |       |       |       |               |       |       |       |
+ *                         +-------+-------+-------+               +-------+-------+-------+
+ */
+
 [INDX] = LAYOUT_moonlander(
-        _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
-        _______, _______, _______,  _______, _______, KC_ESC,  _______, _______,  KC_BSPC, _______, _______, _______, _______, _______,
-        _______, _______, _______,  _______, _______, KC_TAB,  _______, _______, KC_ENT,  _______, _______, _______,  _______, _______,
-        _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,  _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
+        _______, _______, _______, KC_MS_U, _______, KC_ESC,  _______, _______, KC_BSPC, _______, KC_UP,   _______,  _______, _______,
+        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_TAB,  _______, _______, KC_ENT,  KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_R, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______
 ),
 
 };
-
-#define LEFT_SHIFT_INDEX 31
-#define RIGHT_SHIFT_INDEX 67
-#define LEFT_CTRL_INDEX 33
-#define RIGHT_CTRL_INDEX 69
-#define LEFT_ALT_INDEX 34
-#define RIGHT_ALT_INDEX 70
-#define LEFT_GUI_INDEX 4
-#define RIGHT_GUI_INDEX 40
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
@@ -147,6 +164,10 @@ void oneshot_locked_mods_changed_user(uint8_t mods) {
   maybe_reset_rgb_matrix(mods);
 }
 
+void reset_color(int index) {
+    rgb_matrix_set_color(index, 0, 0, 0);
+}
+
 bool rgb_matrix_indicators_user(void) {
   uint8_t mods = get_oneshot_mods();
   uint8_t locked_mods = get_oneshot_locked_mods();
@@ -158,8 +179,8 @@ bool rgb_matrix_indicators_user(void) {
     rgb_matrix_set_color(LEFT_SHIFT_INDEX, 255, 0, 0);
     rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 255, 0, 0);
   } else {
-    rgb_matrix_set_color(LEFT_SHIFT_INDEX, 0, 0, 0);
-    rgb_matrix_set_color(RIGHT_SHIFT_INDEX, 0, 0, 0);
+    reset_color(LEFT_SHIFT_INDEX);
+    reset_color(RIGHT_SHIFT_INDEX);
   }
 
   if (mods & MOD_MASK_CTRL) {
@@ -169,8 +190,8 @@ bool rgb_matrix_indicators_user(void) {
     rgb_matrix_set_color(LEFT_CTRL_INDEX, 0, 255, 0);
     rgb_matrix_set_color(RIGHT_CTRL_INDEX, 0, 255, 0);
   } else {
-    rgb_matrix_set_color(LEFT_CTRL_INDEX, 0, 0, 0);
-    rgb_matrix_set_color(RIGHT_CTRL_INDEX, 0, 0, 0);
+    reset_color(LEFT_CTRL_INDEX);
+    reset_color(RIGHT_CTRL_INDEX);
   }
 
   if (mods & MOD_MASK_ALT) {
@@ -180,8 +201,8 @@ bool rgb_matrix_indicators_user(void) {
     rgb_matrix_set_color(LEFT_ALT_INDEX, 0, 0, 255);
     rgb_matrix_set_color(RIGHT_ALT_INDEX, 0, 0, 255);
   } else {
-    rgb_matrix_set_color(LEFT_ALT_INDEX, 0, 0, 0);
-    rgb_matrix_set_color(RIGHT_ALT_INDEX, 0, 0, 0);
+    reset_color(LEFT_ALT_INDEX);
+    reset_color(RIGHT_ALT_INDEX);
   }
 
   if (mods & MOD_MASK_GUI) {
@@ -191,8 +212,8 @@ bool rgb_matrix_indicators_user(void) {
     rgb_matrix_set_color(LEFT_GUI_INDEX, 100, 200, 255);
     rgb_matrix_set_color(RIGHT_GUI_INDEX, 100, 200, 255);
   } else {
-    rgb_matrix_set_color(LEFT_GUI_INDEX, 0, 0, 0);
-    rgb_matrix_set_color(RIGHT_GUI_INDEX, 0, 0, 0);
+    reset_color(LEFT_GUI_INDEX);
+    reset_color(RIGHT_GUI_INDEX);
   }
 
   return true;
